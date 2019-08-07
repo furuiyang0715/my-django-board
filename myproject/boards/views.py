2f4d00ca027c511da5aca66f78745f381fd64fb2
furuiyang <furuiyang0715@gmail.com>
2019-08-07 10:37:38 +0800

    第一个视图函数

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
 from django.http import HttpResponse
+from .models import Board
 
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    # 导入 Board 模型并且列出所以的板块
+    boards = Board.objects.all()
+    boards_names = list()
+
+    for board in boards:
+        boards_names.append(board.name)
+
+    response_html = '<br>'.join(boards_names)
+
+    return HttpResponse(response_html)
