be0ce6333a700fae8e8387aaa64ece52679f06f6
furuiyang <furuiyang0715@gmail.com>
2019-08-07 14:20:30 +0800

    完善注册的后台业务逻辑

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -1,7 +1,15 @@
+from django.contrib.auth import login as auth_login  # login函数重命名为auth_login以避免与内置login视图冲突
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
