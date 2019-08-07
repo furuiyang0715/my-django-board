2bcc535b1ad81477d362d12846b387ffb3b4997f
furuiyang <furuiyang0715@gmail.com>
2019-08-07 11:22:46 +0800

    静态文件设置

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 设置项目静态文件的位置
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+]
