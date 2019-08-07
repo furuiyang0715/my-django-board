c6441562f0ba817dc09686c9b1c1f32ab2ff748d
furuiyang <furuiyang0715@gmail.com>
2019-08-07 10:31:48 +0800

    第五章模型设计

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Board(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        # 定义在交互模式中的显示
+        return self.name
+
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=255)
+    last_updated = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(Board, related_name='topics')
+    starter = models.ForeignKey(User, related_name='topics')
+
+
+class Post(models.Model):
+    message = models.TextField(max_length=4000)
+    topic = models.ForeignKey(Topic, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(User, related_name='posts')
+    # 我们不需要关注用户修改过的帖子
+    updated_by = models.ForeignKey(User, null=True, related_name='+')
