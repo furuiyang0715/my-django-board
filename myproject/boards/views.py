d90cbf56add031bea6ed37845d689c16ef20c2ba
furuiyang <furuiyang0715@gmail.com>
2019-08-08 10:13:32 +0800

    控制相同的用户在刷新页面的时候被统计为多次访问

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -121,8 +121,14 @@ class PostListView(ListView):
     paginate_by = 2
 
     def get_context_data(self, **kwargs):
-        self.topic.views += 1
-        self.topic.save()
+        # 使用会话
+        # 控制相同的用户在刷新页面的时候被统计为多次访问
+        session_key = 'viewed_topic_{}'.format(self.topic.pk)
+        if not self.request.session.get(session_key, False):
+            self.topic.views += 1
+            self.topic.save()
+            self.request.session[session_key] = True
+
         kwargs['topic'] = self.topic
         return super().get_context_data(**kwargs)
 
