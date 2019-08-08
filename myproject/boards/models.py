e720c391774da5318e727f3ce9d5ee95b1cb8e17
furuiyang <furuiyang0715@gmail.com>
2019-08-08 09:45:47 +0800

    增减 markdown 支持

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
 from django.utils.text import Truncator
+from markdown import markdown
 
 
 class Board(models.Model):
@@ -41,3 +43,6 @@ class Post(models.Model):
     def __str__(self):
         truncated_message = Truncator(self.message)
         return truncated_message.chars(30)
+
+    def get_message_as_markdown(self):
+        return mark_safe(markdown(self.message, safe_mode='escape'))
